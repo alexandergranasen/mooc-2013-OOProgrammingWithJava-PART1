4ec6177e8fabc2695936cbf80fab364ea53d5c61
Alexander Granåsen <alexander.granasen@gmail.com>
2019-09-21 09:54:25 +0200

    Completed exercise 2

---
+++ week1-002.HelloWorld/src/Greeter.java
@@ -0,0 +1,10 @@
+public class Greeter {
+
+    public static void main(String[] args) {
+        // Write your code here
+        // Program that greets the user
+        System.out.println("Hello world!");
+        System.out.println("(And all the people of the world)");
+    }
+
+}

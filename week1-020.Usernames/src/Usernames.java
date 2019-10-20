edb3f0d1672011b331eae5373d3662f1aacb66a7
Alexander <alexander.granasen@gmail.com>
2019-10-20 16:46:54 +0200

    Completed excercise 15-20

---
+++ week1-020.Usernames/src/Usernames.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Usernames {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String username;
+        String password;
+        
+        System.out.print("Type your username: ");
+        username = reader.nextLine();
+        System.out.print("Type your password: ");
+        password = reader.nextLine();
+        
+        if(username.equals("alex") && password.equals("mightyducks")){
+            System.out.println("You are now logged into the system!");
+        } else if(username.equals("emily") && password.equals("cat")){
+            System.out.println("You are now logged into the system!");
+        } else {
+            System.out.println("Your username or password was invalid!");
+        }
+    }
+}
